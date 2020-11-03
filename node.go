2ffa1feb8ab1c4843ef25a1a9937c34c2c3892d2
templexxx <lucas1x1x@gmail.com>
2020-11-03 17:19:23 +0800

    node: raw header struct

--- node.go
+++ node.go
@@ -8,7 +8,13 @@ const (
 	HeaderLen  = VersionLen + ParentLen
 )
 
+// Node is the basic structure of art.
 type Node struct {
+	// Header includes basic information of a node:
+	// Struct:
+	// +-----------+-------------+---------+------------+----------+----------+
+	// | offset(8) | prefix_len(4) |  type(3) | old(1) | locked(1) | expand(1) | vexpand(8)
+	// +-----------+-------------+---------+------------+----------+----------+
 	Header uint32
 	Prefix [8]byte
 	Parent unsafe.Pointer

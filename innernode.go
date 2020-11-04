a43cf44eb36d786bb56c740cad07ab5e650e7052
templexxx <lucas1x1x@gmail.com>
2020-11-04 17:35:18 +0800

    innernode: add more node types for saving memory

--- innernode.go
+++ innernode.go
@@ -2,31 +2,37 @@ package artree
 
 import "unsafe"
 
-type Node4 struct {
+type Node2 struct {
 	*Node
-	keys     [4]byte
-	children [4]unsafe.Pointer
+	keys     [2]byte
+	children *[2]unsafe.Pointer
+}
+
+type Node5 struct {
+	*Node
+	keys     [5]byte
+	children *[5]unsafe.Pointer
 }
 
 type Node16 struct {
 	*Node
 	keys     [16]byte
-	children [16]unsafe.Pointer
+	children *[16]unsafe.Pointer
 }
 
 type Node32 struct {
 	*Node
-	keys     [32]byte
-	children [32]unsafe.Pointer
+	indexes  [16]byte
+	children *[32]unsafe.Pointer
 }
 
-type Node48 struct {
+type Node64 struct {
 	*Node
-	indexes  [48]byte
-	children [48]unsafe.Pointer
+	indexes  [64]byte
+	children *[64]unsafe.Pointer
 }
 
 type Node256 struct {
 	*Node
-	children [256]unsafe.Pointer
+	children *[256]unsafe.Pointer
 }
--- node.go
+++ node.go
@@ -11,10 +11,10 @@ const (
 // Node is the basic structure of art.
 type Node struct {
 	// Header includes basic information of a node:
-	// Struct:
-	// +-----------+-------------+---------+------------+----------+----------+
-	// | offset(8) | prefix_len(4) |  type(3) | old(1) | locked(1) | expand(1) | vexpand(8)
-	// +-----------+-------------+---------+------------+----------+----------+
+	// Struct(from lowest bit to highest bit):
+	// +---------+------+--------+-------------+--------+----------+----------+
+	// | type(3) | p(1) | off(8) | pref_len(4) | old(1) | locked(1) | expand(1) | vexpand(8)
+	// +---------+------+--------+-------------+--------+----------+----------+
 	Header uint32
 	Prefix [8]byte
 	Parent unsafe.Pointer

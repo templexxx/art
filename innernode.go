87de04615a1a8a39d55450842ca9df52641ca9eb
templexxx <lucas1x1x@gmail.com>
2020-11-10 15:05:50 +0800

    innernode: add new header

--- innernode.go
+++ innernode.go
@@ -3,36 +3,36 @@ package artree
 import "unsafe"
 
 type Node2 struct {
-	*Node
 	keys     [2]byte
 	children *[2]unsafe.Pointer
+	header   *byte
 }
 
 type Node5 struct {
-	*Node
 	keys     [5]byte
 	children *[5]unsafe.Pointer
+	header   *byte
 }
 
 type Node16 struct {
-	*Node
 	keys     [16]byte
 	children *[16]unsafe.Pointer
+	header   *byte
 }
 
 type Node32 struct {
-	*Node
-	indexes  [16]byte
+	indexes  [32]byte
 	children *[32]unsafe.Pointer
+	header   *byte
 }
 
 type Node64 struct {
-	*Node
-	indexes  [64]byte
+	indexes  [256]byte
 	children *[64]unsafe.Pointer
+	header   *byte
 }
 
 type Node256 struct {
-	*Node
 	children *[256]unsafe.Pointer
+	header   *byte
 }

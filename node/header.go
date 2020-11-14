d48080f895bd00aa276799586a7b66579b968d16
templexxx <lucas1x1x@gmail.com>
2020-11-14 10:27:40 +0800

    node: header functions done

--- node/header.go
+++ node/header.go
@@ -35,10 +35,11 @@ const (
 )
 
 // makeNodeHeader makes a new header with specified node type and its level.
+//
 // level won't change in node's whole life.
-func makeNodeHeader(nodeType, level uint32) *byte {
+func makeNodeHeader(nodeType uint8, level uint32) *byte {
 	p := mem.MakeAlignedBlock(16, 16)
-	n := nodeType | level<<5
+	n := uint32(nodeType) | level<<5
 	binary.LittleEndian.PutUint32(p[:4], n)
 	return &p[0]
 }
@@ -83,4 +84,40 @@ func getLevel(h *byte) uint32 {
 
 	hb := (*[16]byte)(unsafe.Pointer(h))
 	return binary.LittleEndian.Uint32((*hb)[:4]) >> 5 & (1<<23 - 1)
+}
+
+// getPrefixLen gets node prefix length.
+// h must be returned by func load.
+func getPrefixLen(h *byte) (prefixLen uint8) {
+
+	hb := (*[16]byte)(unsafe.Pointer(h))
+	return uint8(binary.LittleEndian.Uint32((*hb)[:4]) >> 28 & (1<<4-1))
+}
+
+// getPrefix gets node prefix.
+// h must be returned by func load.
+func getPrefix(h *byte, pLen uint8) []byte {
+
+	if pLen == 0 {
+		return nil
+	}
+
+	hb := (*[16]byte)(unsafe.Pointer(h))
+	return (*hb)[4:4+pLen]
+}
+
+// setPrefix sets node prefix.
+// h is the origin header address.
+// old is the loaded header address.
+func setPrefix(h, old *byte, prefix []byte) bool {
+
+	oldb := (*[16]byte)(unsafe.Pointer(old))
+	oldPrefixLen := binary.LittleEndian.Uint32((*oldb)[:4]) >> 28 & (1<<4-1)
+	newb := *oldb
+	prefixLen := len(prefix)
+	tmpU32 := (binary.LittleEndian.Uint32(newb[:4]) ^ (oldPrefixLen<<28)) | (uint32(prefixLen) << 28)
+	binary.LittleEndian.PutUint32(newb[:4], tmpU32)
+	copy(newb[4:], prefix)
+
+	return mem.AtomicCAS16B(h, old, &newb[0])
 }
--- node/header_test.go
+++ node/header_test.go
@@ -1,16 +1,30 @@
 package node
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestNewHeader(t *testing.T) {
-	h := makeNodeHeader(node2Type, 0)
-	if getNodeType(h) != node2Type {
-		t.Fatal("new header node type mismatch")
+func TestMakeNodeHeader(t *testing.T) {
+
+	for i := 0; i < 256; i++ {
+		for j := node2Type; j <= node256Type; j++ {
+			h := makeNodeHeader(uint8(j), uint32(i))
+
+			hl := load(h)
+
+			if getNodeType(hl) != uint8(j) {
+				t.Fatal("new header node type mismatch")
+			}
+			if getLevel(hl) != uint32(i) {
+				t.Fatal("new header level mismatch")
+			}
+		}
 	}
 }
 
 func TestIsObsolete(t *testing.T) {
-	h := makeNodeHeader(node2Type, 0)
+	h := makeNodeHeader(node2Type, 1)
 	hl := load(h)
 
 	if isObsolete(hl) {
@@ -24,15 +38,52 @@ func TestIsObsolete(t *testing.T) {
 	if !isObsolete(h) {
 		t.Fatal("node should be obsolete")
 	}
+
+	hl = load(h)
+	if getNodeType(hl) != node2Type {
+		t.Fatal("node type mismatch")
+	}
+	if getLevel(hl) != 1 {
+		t.Fatal("node level mismatch")
+	}
+	if !isObsolete(hl) {
+		t.Fatal("node should be obsolete")
+	}
 }
 
-func TestGetLevel(t *testing.T) {
-	for i := 0; i < 128; i++ {
-		h := makeNodeHeader(node2Type, uint32(i))
-		lvl := getLevel(h)
+func TestSetPrefix(t *testing.T) {
+	h := makeNodeHeader(node2Type, 3)
 
-		if lvl != uint32(i) {
-			t.Fatal("level mismatch")
-		}
+	hl := load(h)
+	if !setPrefix(h, hl, []byte{1, 2}) {
+		t.Fatal("set prefix should be ok")
+	}
+	pl := getPrefixLen(hl)
+	if pl != 0 {
+		t.Fatal("prefix length mismatch")
+	}
+	prefix := getPrefix(hl, pl)
+	if prefix != nil {
+		t.Fatal("prefix mismatch", prefix)
+	}
+
+	hl = load(h)
+	pl = getPrefixLen(hl)
+	if pl != 2 {
+		t.Fatal("prefix length mismatch")
+	}
+	prefix = getPrefix(hl, pl)
+	if !bytes.Equal(prefix, []byte{1,2}) {
+		t.Fatal("prefix mismatch")
+	}
+
+	if getNodeType(hl) != node2Type {
+		t.Fatal("node type mismatch")
+	}
+	if getLevel(hl) != 3 {
+		t.Fatal("node level mismatch")
+	}
+	if isObsolete(hl) {
+		t.Fatal("node should not be obsolete")
 	}
 }
